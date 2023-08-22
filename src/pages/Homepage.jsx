c342bc2604b2c8a9e4dca3c469d6e872faa8cadc
Giada15 <tascheriogiada@gmail.com>
2023-08-22 10:27:36 -0400

    Add CSS Modules and additional initial structure
    In this commit:
    - Added additional initial files (starter folder)
    - Created global CSS file (index.css)
    - Implemented PageNav and AppNav CSS modules for component styles
    - Added AppLayout component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,16 @@
+import AppNav from "../components/AppNav"
 import PageNav from "../components/PageNav"
+import { Link } from "react-router-dom"
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+
+      <AppNav />
       <h1>Worldwise</h1>
+
+      <Link to="/app">Go to the App</Link>
     </div>
   )
 }
