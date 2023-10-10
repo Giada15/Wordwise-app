6d263061922f76c51a1dc1b9276ca4323a30c2c0
Giada15 <tascheriogiada@gmail.com>
2023-10-10 10:19:15 -0400

    Implement user authentication
    In this commit:
    - Implemented a 'login' function in the 'Login' component that navigates to '/app' when the user is logged in.
    - Displayed the logged-in user in the 'User' component.
    - Implemented a 'logout' function in the 'User' component that navigates back to '/'.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,27 +1,34 @@
-import styles from "./User.module.css";
+import { useEffect } from "react"
+import { useAuth } from "../contexts/FakeAuthContext"
+import styles from "./User.module.css"
+import { useNavigate } from "react-router-dom"
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+// const FAKE_USER = {
+//   name: "Jack",
+//   email: "jack@example.com",
+//   password: "qwerty",
+//   avatar: "https://i.pravatar.cc/100?u=zz",
+// }
 
 function User() {
-  const user = FAKE_USER;
+  const { user, logout, isAuthenticated } = useAuth()
+  const navigate = useNavigate()
 
-  function handleClick() {}
+  function handleClick() {
+    logout()
+    navigate("/")
+  }
 
   return (
     <div className={styles.user}>
-      <img src={user.avatar} alt={user.name} />
-      <span>Welcome, {user.name}</span>
+      <img src={user && user.avatar} alt={user && user.name} />
+      <span>Welcome, {user && user.name}</span>
       <button onClick={handleClick}>Logout</button>
     </div>
-  );
+  )
 }
 
-export default User;
+export default User
 
 /*
 CHALLENGE
